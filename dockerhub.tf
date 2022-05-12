1acbb478dd5ce06297f105ab2abde9d8ebd1eab7
bolinz <dev@bolinz.com>
2022-05-13 00:52:06 +0800

    update dockerhub provider

---
+++ dockerhub.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "magenta-aps/dockerhub"
+      version = ">= 0.0.12"
+    }
+  }
+}
+
+provider "dockerhub" {
+  # Note: This cannot be a Personal Access Token
+  username = var.dockerhub_user # or use DOCKER_USERNAME environment variable
+  password = var.dockerhub_token # or use DOCKER_PASSWORD environment variable
+}
+
+
+resource "dockerhub_repository" "example" {
+  name             = "example"
+  namespace        = "bolinz"
+  description      = "Example repository."
+  full_description = "Readme."
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "dockerhub_user" {
+  type    = string
+  default = ["us-west-1a"]
+}
+variable "dockerhub_token" {
+  type        = string
+  default     = ""
+  description = "description"
+}
